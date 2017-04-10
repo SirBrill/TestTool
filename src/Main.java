053d5d7f0a8272971b0f92f40a8569a1cd5e4e80
SirBrill <SirBrill@localhost>
2017-04-09 21:28:08 -0400

    Initial check-in of test tool

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	public static void main(String[] args ) {
+		TestData data = new TestData();
+		
+		displayGreeting();
+		data.display();
+		
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("Hello, test people");
+		System.out.println("This is a test");
+	}
+
+}
---
+++ src/TestData.java
@@ -0,0 +1,9 @@
+
+public class TestData {
+
+	int data[] = {0, 4, 42};
+	
+	public void display() {
+		System.out.println("TODO: Implement test display");
+	}
+}
